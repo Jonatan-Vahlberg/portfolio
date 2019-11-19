6021dfd2742153539e7addcce7d81d1d093da9af
Jonatan Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2019-11-19 20:19:01 +0100

    Updates

---
+++ precache-manifest.c9f784f0bf239b0f7ee679326c7c16b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f1dd17609707bd1991040642966360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1b6d15c7cee90d1cd1",
+    "url": "/static/css/main.2522232f.chunk.css"
+  },
+  {
+    "revision": "942829f49eb00b648cfb",
+    "url": "/static/js/2.f249b60b.chunk.js"
+  },
+  {
+    "revision": "ac1b6d15c7cee90d1cd1",
+    "url": "/static/js/main.de2d3244.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "6b839283bf0f6200f0887b110eae8875",
+    "url": "/static/media/MYLI16.6b839283.png"
+  },
+  {
+    "revision": "115b1826312e7eb157858b2fd38d9275",
+    "url": "/static/media/headshot_small.115b1826.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "bf6f26c1bc32568db5525f55c2392300",
+    "url": "/static/media/spotitheme.bf6f26c1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.89478f47db845f9d8b3b21b740920db4.js"
+  "/precache-manifest.c9f784f0bf239b0f7ee679326c7c16b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
