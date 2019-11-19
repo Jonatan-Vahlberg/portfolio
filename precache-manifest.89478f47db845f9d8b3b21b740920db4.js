183ba219e552ad582cdd269aea47868c311e9e86
Jonatan Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2019-11-19 19:34:44 +0100

    Updates

---
+++ precache-manifest.89478f47db845f9d8b3b21b740920db4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf963529c7b24d3fa9a20402e537aff",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "81a96f6053d5ce175d00",
+    "url": "/portfolio/static/css/main.2522232f.chunk.css"
+  },
+  {
+    "revision": "942829f49eb00b648cfb",
+    "url": "/portfolio/static/js/2.f249b60b.chunk.js"
+  },
+  {
+    "revision": "81a96f6053d5ce175d00",
+    "url": "/portfolio/static/js/main.2d5a1d40.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "6b839283bf0f6200f0887b110eae8875",
+    "url": "/portfolio/static/media/MYLI16.6b839283.png"
+  },
+  {
+    "revision": "115b1826312e7eb157858b2fd38d9275",
+    "url": "/portfolio/static/media/headshot_small.115b1826.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "bf6f26c1bc32568db5525f55c2392300",
+    "url": "/portfolio/static/media/spotitheme.bf6f26c1.png"
+  }
+]);
