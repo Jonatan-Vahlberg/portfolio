ff181cbb813ee7a5209d027cb4ea306ff6585c6c
Jonatan Ramses Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2020-01-29 15:46:15 +0100

    added new project

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,72 +4,80 @@ import Fade from 'react-reveal/Fade';
 
 import myli from '../media/MYLI16.png';
 import spoti from '../media/spotitheme.png';
+import qrlib from '../media/QRLIB.png';
 
 const Project = ({
-    url,
-    imageSrc,
-    title,
-    service,
-    desc,
-    contact = null,
-    gitHub = null
+  urlLive = null,
+  url,
+  imageSrc,
+  title,
+  service,
+  desc,
+  contact = null,
+  github = null,
 }) => {
-    return (
-        <Fade bottom>
-            <div className='project'>
-                <a href={url}>
-                    <img
-                        className='project-image'
-                        src={getCorrectImage(imageSrc)}
-                    />
-                </a>
-                <h1>{title}</h1>
-                <span>{service}</span>
-                <p>{desc}</p>
-                {gitHub !== null && (
-                    <div className='header'>
-                        <button>
-                            <a
-                                href={gitHub}
-                                rel='noopener noreferrer'
-                                target='_blank'>
-                                Check out on GitHub!
-                            </a>
-                        </button>
-                    </div>
-                )}
-                {contact !== null && (
-                    <div>
-                        <p>
-                            Name: <b>{contact.name}</b>, {contact.title}
-                        </p>
-                        <p>
-                            {'Mail: '}
-                            <a
-                                href={`mailto:${contact.mail}`}
-                                rel='noopener noreferrer'>
-                                <b>{contact.mail}</b>
-                            </a>
-                        </p>
-                        <p>
-                            Phone: <b>{contact.phone}</b>
-                        </p>
-                    </div>
-                )}
-            </div>
-        </Fade>
-    );
+  console.log(title);
+  console.log(github);
+
+  return (
+    <Fade bottom>
+      <div className="project">
+        <a href={url}>
+          <img className="project-image" src={getCorrectImage(imageSrc)} />
+        </a>
+        <h1>{title}</h1>
+        <span>{service}</span>
+        <p>{desc}</p>
+        {github !== null && (
+          <div className="header">
+            <button style={{ background: '#9352ae' }}>
+              <a href={github} rel="noopener noreferrer" target="_blank">
+                Check out on GitHub!
+              </a>
+            </button>
+          </div>
+        )}
+        {urlLive !== null && (
+          <div id="urlButton" className="header" style={{ marginTop: 20 }}>
+            <button>
+              <a href={url} rel="noopener noreferrer" target="_blank">
+                Check out live version!
+              </a>
+            </button>
+          </div>
+        )}
+        {contact !== null && (
+          <div>
+            <p>
+              Name: <b>{contact.name}</b>, {contact.title}
+            </p>
+            <p>
+              {'Mail: '}
+              <a href={`mailto:${contact.mail}`} rel="noopener noreferrer">
+                <b>{contact.mail}</b>
+              </a>
+            </p>
+            <p>
+              Phone: <b>{contact.phone}</b>
+            </p>
+          </div>
+        )}
+      </div>
+    </Fade>
+  );
 };
 
 const getCorrectImage = title => {
-    switch (title) {
-        case 'myli':
-            return myli;
-        case 'spoti':
-            return spoti;
-        default:
-            return myli;
-    }
+  switch (title) {
+    case 'myli':
+      return myli;
+    case 'spoti':
+      return spoti;
+    case 'qrlib':
+      return qrlib;
+    default:
+      return myli;
+  }
 };
 
 export default Project;
--- src/data.js
+++ src/data.js
@@ -1,65 +1,79 @@
 export default {
-    name: {
-        first: 'Jonatan',
-        last: 'Vahlberg'
-    },
-    headerTagline: [
-        //Line 1 For Header
-        'Jonatan Vahlberg.',
-        //Line 2 For Header
-        'App Developer, Student ',
-        //Line 3 For Header
-        'from Sweden'
-    ],
-    contactEmail: 'jonatan.ramses.vahlberg@gmail.com',
-    abouttext: `I am a Swedish programmer from Stockholm, currently enrolled in a two year Application development course hosted by STI (Stockholm Institute of Technology).
+  name: {
+    first: 'Jonatan',
+    last: 'Vahlberg',
+  },
+  headerTagline: [
+    //Line 1 For Header
+    'Jonatan Vahlberg.',
+    //Line 2 For Header
+    'App Developer, Student ',
+    //Line 3 For Header
+    'from Sweden',
+  ],
+  contactEmail: 'jonatan.ramses.vahlberg@gmail.com',
+  abouttext: `I am a Swedish programmer from Stockholm, currently enrolled in a two year Application development course hosted by STI (Stockholm Institute of Technology).
          The main goal with the course is to give budding programers a more real program closely tied with the current tech industry. 
          The course helps us develop a lot of different skills such as  "HTML, CSS, SCCS, Javascript and NodeJS" for web development. "IOS/Swift, Java, xamarin and React-Native" 
          for more native and hybrid mobile development. Alongside these skills we also develop a sense of UI/UX, 
          good clean coding, debugging, avoiding antipatterns, light database handling, working with APIs and perhaps most importantly, 
          internships with real companies, where we get to further develop our skills in real world projects.`,
-    aboutImage: './src/media/headshot.jpg',
-    ShowAboutImage: true,
+  aboutImage: './src/media/headshot.jpg',
+  ShowAboutImage: true,
 
-    projects: [
-        {
-            id: 1,
-            title: 'MyLi',
-            service: 'React-Native Application',
+  projects: [
+    {
+      id: 1,
+      title: 'MyLi',
+      service: 'React-Native Application',
 
-            imageSrc: 'myli',
+      imageSrc: 'myli',
 
-            url: 'http://myli.se',
-            desc: `During fall 2019 I interned at MyIndicators a digital healthcare focused IT company. During my time there 
+      url: 'http://myli.se',
+      desc: `During fall 2019 I interned at MyIndicators a digital healthcare focused IT company. During my time there 
             i together with a SCRUM team developed an MVP (Minimum Viable Product) in the form of a Android focused React-Native hybrid application. 
             We dealt with everything from state management in Redux to navigation in RNRF while following a design document and a strict backend through an API. 
             This project was the first real project I took part in and it gave me a lot of insight into working for a company with deadlines and a supporting team. 
             If you like to know more about the work I did for MyIndicators you can either contact me or my supervisor at the company.`,
-            contact: {
-                name: 'Patrik Helenius',
-                title: 'Co founder of MyIndicators',
-                mail: 'patrik.helenius@myindicators.se',
-                phone: '+46 702 632 650'
-            }
-        },
-        {
-            id: 2,
-            title: 'Spoti-Theme',
-            service: 'React-Native Application',
-            imageSrc: 'spoti',
-            desc: `Developed in school during a course relating to API connection and handling. Spoti-Theme is a smaller project that grabs 
+      contact: {
+        name: 'Patrik Helenius',
+        title: 'Co founder of MyIndicators',
+        mail: 'patrik.helenius@myindicators.se',
+        phone: '+46 702 632 650',
+      },
+    },
+    {
+      id: 2,
+      title: 'QR Library',
+      service: 'TypeScript React-native',
+      imageSrc: 'qrlib',
+      desc: `QR library is a QR/barcode scanner application. It allows for scanning viewing and saving data from a wide variety of data pionts.
+              It makes a jugement call for wether the data is for example a interpretable international phone number or a link to an image and
+              displays the data depending on match. The application is built in react native trough typescript and utelizes some external libraries for navigation
+              and simmilar aspects. The data is saved completly offline and therefor unreadable in any other way than trough the application.`,
+      url: 'https://expo.io/@jonatan_vahlberg/QR_Library',
+      urlLive: 'https://expo.io/@jonatan_vahlberg/QR_Library',
+      github: 'https://github.com/Jonatan-Vahlberg/QRLibrary',
+    },
+    {
+      id: 3,
+      title: 'Spoti-Theme',
+      service: 'React-Native Application',
+      imageSrc: 'spoti',
+      desc: `Developed in school during a course relating to API connection and handling. Spoti-Theme is a smaller project that grabs 
             playlists from spotify's public API and through a Firebase database saves your preferences. The project was built over two weeks in early fall 2019. 
             It was built using Expo's fantastic React-Native wrapper and Firebase Cloud Firestore as it's database.`,
-            url: 'https://expo.io/@jonatan_vahlberg/spoty-theme',
-            gitHub: 'https://github.com/Jonatan-Vahlberg/spoti-theme'
-        }
-    ],
-    social: [
-        { name: 'Email', url: 'https://jonatan.ramses.vahlberg@gmail.com' },
-        { name: 'Github', url: 'https://github.com/Jonatan-Vahlberg' },
-        {
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/in/jonatan-vahlberg/'
-        }
-    ]
+      url: 'https://expo.io/@jonatan_vahlberg/spoty-theme',
+      urlLive: 'https://expo.io/@jonatan_vahlberg/spoty-theme',
+      github: 'https://github.com/Jonatan-Vahlberg/spoti-theme',
+    },
+  ],
+  social: [
+    { name: 'Email', url: 'https://jonatan.ramses.vahlberg@gmail.com' },
+    { name: 'Github', url: 'https://github.com/Jonatan-Vahlberg' },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/jonatan-vahlberg/',
+    },
+  ],
 };
